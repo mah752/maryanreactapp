4d11443f579da4b15fe199f1aa13a939f1e5de85
mah752 <83982869+mah752@users.noreply.github.com>
2022-03-05 10:30:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
+import "react"
+from React;
 import "./App.css";
 import SearchEngine from "./SearchEngine";
-import FancyLoadingAnimation from "fancy-loading-animation";
-import "fancy-loading-animation/dist/index.css";
+
 
 function App() {
     return ( <
         div className = "App" >
         <
-        FancyLoadingAnimation loadingAnimationVarient = "bee"
-        loadingAnimationDelay = { 0.3 }
-        loadingCharacterDirection = "right"
-        loadingTextStyle = {
-            { color: "orange" }
-        }
-        />{" "} <
         header className = "App-header" >
         <
         h1 > What 's the weather today? </h1> <SearchEngine />{" "} <
